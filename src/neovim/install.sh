924d180bec7a34d38c17f46cda39ebc7b0d9021d
Tim Smith <2762951+tahsmith@users.noreply.github.com>
2023-10-28 12:46:40 +0000

    working test

--- src/neovim/install.sh
+++ src/neovim/install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+apt-get update
+apt-get install -y wget tar
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
 
-tar -C /usr -xzf nvim-linux64.tar.gz
+tar -xzf nvim-linux64.tar.gz
+cp -r nvim-linux64/* /usr
